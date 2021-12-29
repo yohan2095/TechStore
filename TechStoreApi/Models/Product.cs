0923bbc3f93933c784d79501fa594da2e23c4db7
Yohan Arejouane <yohan.arejouane@icloud.com>
2021-12-29 23:54:27 +0200

    v0.1
    First Commit

---
+++ TechStoreApi/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace TechStoreApi.Models
+{
+	public class Product
+	{
+		[BsonId]
+		[BsonRepresentation(BsonType.ObjectId)]
+		public string? Id { get; set; }
+
+		[BsonElement("Name")]
+		public string ProductName { get; set; } = null!;
+
+		public decimal Price { get; set; }
+
+		public string Category { get; set; } = null!;
+
+		public string Firm { get; set; } = null!;
+	}
+}
+
---
+++ TechStoreApi/Models/TechStoreDatabaseSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace TechStoreApi.Models
+{
+	public class TechStoreDatabaseSettings
+	{
+		public string ConnectingString { get; set; } = null!;
+
+		public string DatabaseName { get; set; } = null!;
+
+		public string ProductsCollectionName { get; set; } = null!;
+	}
+}
+
